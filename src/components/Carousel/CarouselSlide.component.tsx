6c7ba86d43860ace1f6ece7bd023c91aa9726c40
kkamburov@bitbucket.org <kamburov.kaloyan@gmail.com>
2024-10-09 20:01:32 +0300

    build fix attempt

--- src/components/Carousel/CarouselSlide.component.tsx
+++ src/components/Carousel/CarouselSlide.component.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { FC, useState, useEffect } from "react";
+import { FC, useState } from "react";
 import { CarouselSlideProps } from "./Carousel.types";
 
 const CarouselSlide: FC<CarouselSlideProps> = ({
